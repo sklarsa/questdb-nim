2ee120a50e18731b8bf2f8a1334d06e28b3447da
Steve Sklar <steven@questdb.io>
2022-09-29 14:17:37 -0400

    Fix test

--- questdb_nim/tests/test_protocol.nim
+++ questdb_nim/tests/test_protocol.nim
@@ -8,17 +8,19 @@ import message
 test "protocol":
 
   let symbols = {"t1":"tv1", "t2":"tv2"}.toOrderedTable()
-  let timestamp = now().toTime()
+  let time = now().toTime()
+  let timeMicroseconds = time.toMicroseconds()
+  let timeNanoseconds = time.toNanoseconds()
 
   let vals = {
     "v1": IlpValue(kind: ilpFloat, floatVal: 1.0),
     "v2": IlpValue(kind: ilpString, stringVal: "2.0"),
     "v3": IlpValue(kind: ilpInt, intVal: 3),
-    "v4": IlpValue(kind: ilpTime, timeVal: timestamp),
+    "v4": IlpValue(kind: ilpTime, timeVal: time),
     "v5": IlpValue(kind: ilpBool, boolVal: false),
   }.toOrderedTable()
 
-  let time = now().toTime()
+
 
   # Message with no timestamp
   let m1 = IlpMessage(
@@ -26,8 +28,7 @@ test "protocol":
       symbolset: symbols,
       columnset: vals,
   )
-  let t1 = $(vals["v4"].timeVal.toUnixFloat()) & 't'
-  check $m1 == &"hi,t1=tv1,t2=tv2 v1=1.0,v2=\"2.0\",v3=3i,v4={t1},v5=false"
+  check $m1 == &"hi,t1=tv1,t2=tv2 v1=1.0,v2=\"2.0\",v3=3i,v4={timeMicroseconds}t,v5=false"
   m1.validate()
 
   # Message with a timestamp
@@ -37,7 +38,7 @@ test "protocol":
     columnset: {"v1": IlpValue(kind: ilpString, stringVal: "test")}.toOrderedTable(),
     timestamp: time,
   )
-  check $m2 == "hi,t1=tv1,t2=tv2 v1=\"test\" " & $(m2.timestamp.toUnixFloat())
+  check $m2 == &"hi,t1=tv1,t2=tv2 v1=\"test\" {timeNanoseconds}"
   m2.validate()
 
   # Edge case
